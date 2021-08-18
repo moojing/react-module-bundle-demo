0d307945534d8c737be1a861b0e9dd8d6e8670bc
moojing <starts789@gmail.com>
2021-08-19 00:52:05 +0800

    remove unnecessary files.

--- webpack/karma.entry.js
+++
@@ -1,3 +0,0 @@
-const context = require.context('./src/js/', true, /.+\.test\.jsx?$/);
-context.keys().forEach(context);
-module.exports = context;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,67 @@
-require('babel-register');
-const config = require('./config/index').default;
-module.exports = require('./config/webpack/' + config.get('globals').NODE_ENV).default;
+// storybook specific configuration
+// some how storybook is still using webpack@1.*, come on!
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'file-loader?name=[hash].[ext]',
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss|\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEVELOPMENT__: true,
+      __CLIENT__: true,
+      __SERVER__: false
+    }),
+  ]
+}
